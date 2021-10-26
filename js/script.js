a6102f72dd14cd98bb943e5aa27ffe022b343bf2
Shokhboz-2299 <shokhbozkaumutov@gmail.com>
2021-10-26 21:33:58 +0500

    Update templete

--- js/script.js
+++ js/script.js
@@ -21,12 +21,17 @@ function usersArr(arr) {
   arr.forEach(obj => {
     function renderUsersApi(obj) {
       let user = elUserTemplete.cloneNode(true);
+      
       user.querySelector(".full-name").textContent = `Full Name: ${obj.name}`;
       user.querySelector(".user-name").textContent = `${obj.username}`;
       user.querySelector(".email-address").href = `mailto:${obj.email}`;
       user.querySelector(".email-address").textContent =`${obj.email}`;
       user.querySelector(".phone-number").href = `${obj.phone}`;
       user.querySelector(".phone-number").textContent =`Phone number: ${obj.phone}`;
+      user.querySelector(".user-website").href =`https://${obj.website}`;
+      user.querySelector(".user-website").textContent =`${obj.website}`;
+      user.querySelector(".user-company").textContent = `name: ${obj.company.name}; catchPhrase: ${obj.company.catchPhrase}`;
+      user.querySelector(".user-address").textContent = `street: ${obj.address.street}; city: ${obj.address.city}`;
       user.querySelector(".btn-posts-js").setAttribute("data-id", `${obj.id}`);
       UserFragment.appendChild(user);
     }
